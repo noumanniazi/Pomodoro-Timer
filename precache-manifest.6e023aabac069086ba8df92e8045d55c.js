37f1709790e815bd4cf7518b16f4bf69dc4a4a4c
noumanniazi <noumanaamirkhan@gmail.com>
2019-08-21 15:41:49 +0500

    Updates

---
+++ precache-manifest.6e023aabac069086ba8df92e8045d55c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "526568d9b9afe9cb9c519261ab4be230",
+    "url": "/Pomodoro-Timer/index.html"
+  },
+  {
+    "revision": "38af1784de78df917a45",
+    "url": "/Pomodoro-Timer/static/css/main.e9d42480.chunk.css"
+  },
+  {
+    "revision": "26b6fe65c42dcbcdb794",
+    "url": "/Pomodoro-Timer/static/js/2.425cc01b.chunk.js"
+  },
+  {
+    "revision": "38af1784de78df917a45",
+    "url": "/Pomodoro-Timer/static/js/main.05d01f09.chunk.js"
+  },
+  {
+    "revision": "6c6e9ffef1bfa4fd9141",
+    "url": "/Pomodoro-Timer/static/js/runtime~main.6b71dc51.js"
+  },
+  {
+    "revision": "61eb5a4053f714f2194be9385aa2824d",
+    "url": "/Pomodoro-Timer/static/media/alarm.61eb5a40.mp3"
+  }
+]);
